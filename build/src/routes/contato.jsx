0f1533a8a720229ebcc3539b72616a837880d483
novo <savannapassarela@gmail.com>
2023-09-23 23:02:28 -0300

    up

---
+++ build/src/routes/contato.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contato() {
+  return (
+    <div>
+      <h1>CONTATO</h1>
+      {/* Conteúdo da página de contato */}
+    </div>
+  );
+}
+
+export default Contato;
