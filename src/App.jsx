12fe26b6ab5f0eb4220b912410a037268c7d9ed6
novo <savannapassarela@gmail.com>
2023-09-23 23:09:03 -0300

    Updates

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <p>MEU PROJETO VITE</p>
+
+      <nav>
+        <ul>
+          <li><a href='/' >HOME</a></li>
+          <li><a href='/sobre' >SOBRE</a></li>
+          <li><a href='/contato' >CONTATO</a></li>
+        </ul>
+      </nav>
+     
+      
+    </>
+  )
+}
+
+export default App;
---
+++ src/routes/sobre.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sobre() {
+  return (
+    <div>
+      <h1>SOBRE A EMPRESA</h1>
+      {/* Conteúdo da página sobre a empresa */}
+    </div>
+  );
+}
+
+export default Sobre;
